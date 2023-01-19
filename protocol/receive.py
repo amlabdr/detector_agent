3dca1ed1360b514fba74df7bf34c712694fbd4e1
Abderrahim <amalou.abdr@gmail.com>
2023-01-19 11:43:59 -0500

    add interrupt

--- protocol/receive.py
+++ protocol/receive.py
@@ -42,6 +42,7 @@ class Specification_Receiver_handller(MessagingHandler):
         self.topic = topic
         self.agent = agent
         logging.info("Agent will start listning for spec in the topic: {}".format(self.topic))
+        self.interrupt = False
 
     def on_start(self, event):
         conn = event.container.connect(self.server)
@@ -51,45 +52,61 @@ class Specification_Receiver_handller(MessagingHandler):
         try:
             jsonData = json.loads(event.message.body)
             logging.info("Analyzer will send receipt to the controller")
-            specification = jsonData['specification']
-            parameters = jsonData['parameters']
-            endpoint = jsonData['endpoint']
-            when = jsonData['when']
-            start_time , stop_time, period = when2time(when)
+            if'specification' in jsonData:
+                specification = jsonData['specification']
+                parameters = jsonData['parameters']
+                endpoint = jsonData['endpoint']
+                when = jsonData['when']
+                start_time , stop_time, period = when2time(when)
+                
+                logging.info("specification received for {}".format(specification))
+                logging.info("Agent will send receipt to the controller for {}".format(specification))
             
-            logging.info("specification received for {}".format(specification))
-            logging.info("Agent will send receipt to the controller for {}".format(specification))
-           
-            #agent will publish a receipt for spec
-            specification_receiptData=jsonData.copy()
-            specification_receiptData['receipt'] = jsonData['specification']
-            specification_receiptData["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
-            del specification_receiptData['specification']
-            topic = event.message.reply_to
-            sender.send(self.server,topic, specification_receiptData)
-            logging.info("agent will do the {}".format(specification))
-            result_msg = jsonData.copy()
-            result_msg['result'] = result_msg['specification']
-            del result_msg['specification']
-            #agent will do the spec
-            current_time = time.time()
-            cumulated_seconds = 0
-            while current_time < stop_time:
-                if current_time >= start_time:
-                    resultValues = []
-                    resultValues.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4])
-                    resultValues.append(int(cumulated_seconds*1000))
-                    resultValues.extend(self.agent.run(specification,parameters))
-                    print([type(x) for x in resultValues])
-                    #Agent will send the results to the controller
-                    result_msg["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
-                    result_msg['resultValues'] = [resultValues]
-                    logging.info("Agent will send result {} to the controller".format(result_msg['resultValues']))
-                    result_topic = 'topic://'+endpoint+'/results'
-                    sender.send(self.server,result_topic, result_msg)
-                time.sleep(period/1000)
-                cumulated_seconds +=(time.time() - current_time)
+                #agent will publish a receipt for spec
+                specification_receiptData=jsonData.copy()
+                specification_receiptData['receipt'] = jsonData['specification']
+                specification_receiptData["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
+                del specification_receiptData['specification']
+                topic = event.message.reply_to
+                sender.send(self.server,topic, specification_receiptData)
+                logging.info("agent will do the {}".format(specification))
+                result_msg = jsonData.copy()
+                result_msg['result'] = result_msg['specification']
+                del result_msg['specification']
+                #agent will do the spec
                 current_time = time.time()
+                cumulated_seconds = 0
+                while current_time < stop_time and not(self.interrupt):
+                    if current_time >= start_time:
+                        resultValues = []
+                        resultValues.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4])
+                        resultValues.append(int(cumulated_seconds*1000))
+                        resultValues.extend(self.agent.run(specification,parameters))
+                        print([type(x) for x in resultValues])
+                        #Agent will send the results to the controller
+                        result_msg["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
+                        result_msg['resultValues'] = [resultValues]
+                        logging.info("Agent will send result {} to the controller".format(result_msg['resultValues']))
+                        result_topic = 'topic://'+endpoint+'/results'
+                        sender.send(self.server,result_topic, result_msg)
+                    time.sleep(period/1000)
+                    cumulated_seconds +=(time.time() - current_time)
+                    current_time = time.time()
+                self.interrupt = False
+            elif 'interrupt' in jsonData:
+                logging.info("interruption received")
+                #agent will publish a receipt for interuption
+                interruption_receiptData=jsonData.copy()
+                interruption_receiptData['receipt'] = jsonData['interrupt']
+                interruption_receiptData["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]
+                del specification_receiptData['interrupt']
+                topic = event.message.reply_to
+                sender.send(self.server,topic, specification_receiptData)
+                self.interrupt = True
+            else:
+                pass
+            
+            
 
         except Exception:
             traceback.print_exc()
